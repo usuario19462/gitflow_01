d06d681b32bdc36c191e6fc481a4feeaf033b45e
s3rm4k <s3rm4k@proton.me>
2022-11-09 21:29:19 +0100

    Modificado script menu

--- menu.sh
+++ menu.sh
@@ -1,10 +1,35 @@
 #! /bin/bash
 
-# TODO -> Menu del script
-echo "Linea 1 del MENU"
 
-echo "Linea 2 del MENU"
-
-echo "Linea 3 del MENU"
-
-echo "Linea 4 del MENU"
+while :
+do
+	clear
+	echo " MENU SCRIPT V.0.1 "
+	echo "1. Mostrar directorio"
+	echo "2. Mostrar calendario"
+	echo "3. Mostrar dato"
+	echo "4. Salir"
+	#Escoger menu
+	echo -n "Escoger opcion: "
+	read opcion
+	#Seleccion de menu
+	case $opcion in
+		1) echo "Mostrando directorio"
+		ls
+		read foo
+		;;
+		2) echo "Mostrando calendario"
+		cal
+		read foo
+		;;
+		3) echo "Mostrando datos"
+		date
+		read foo
+		;;
+		4)exit 0;;
+	#Alerta
+	*)echo "Opcion invalida..."
+	sleep 1
+	esac
+done
+		
